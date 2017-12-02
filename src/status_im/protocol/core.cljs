1bfe8bd37a5d1a6f4c483cbb0389f3f1c22c197a
Oskar Thoren <ot@oskarthoren.com>
2017-12-02 20:58:14 +0100

    Under offline inbox flag: add allowp2p for 1on1 filter

--- src/status_im/protocol/core.cljs
+++ src/status_im/protocol/core.cljs
@@ -13,6 +13,7 @@
             [status-im.protocol.encryption :as e]
             [status-im.protocol.discoveries :as discoveries]
             [cljs.spec.alpha :as s]
+            [status-im.utils.config :as config]
             [status-im.utils.random :as random]))
 
 ;; user
@@ -90,11 +91,20 @@
       (let [options (merge listener-options group)]
         (group/start-watching-group! options)))
     ;; start listening to user's inbox
-    (f/add-filter!
-      web3
-      {:key    identity
-       :topics [f/status-topic]}
-      (l/message-listener listener-options))
+    (if config/offline-inbox-enabled?
+      (do (log/info "offline-inbox-enabled, allowp2p")
+          (f/add-filter!
+            web3
+            {:key      identity
+             :allowP2P true
+             :topics   [f/status-topic]}
+            (l/message-listener listener-options)))
+      (f/add-filter!
+        web3
+        {:key    identity
+         :topics [f/status-topic]}
+        (l/message-listener listener-options)))
+
     ;; start listening to profiles
     (doseq [{:keys [identity keypair]} contacts]
       (watch-user! {:web3     web3
